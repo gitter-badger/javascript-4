f3568b9247bd134ed6c6e194422503ffa257d7f1
aksinghdce <aksinghdce@gmail.com>
2016-05-25 09:13:14 +0530

    modal popover

---
+++ modal_popover_sandbox/js/index.js
@@ -0,0 +1,14 @@
+$(document).on('ready', function() {
+  $('#the-text').on('focus', function () {
+    var options = {
+        target: "#external-content",
+        remote: "external.html",
+        placement: "right",
+        backdrop: "false",
+        keyboard: "true"
+    };
+    $('#dialog').modalPopover(options);
+    $('#dialog').modalPopover('show');
+  });
+
+});
