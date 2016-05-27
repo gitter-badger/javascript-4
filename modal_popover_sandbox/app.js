0fa2743f8150393b01dc90b909bfacc5045080e8
aksinghdce <aksinghdce@gmail.com>
2016-05-27 16:24:00 +0530

    CORS problem solved with express server

---
+++ modal_popover_sandbox/app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+//Include static files
+// app.use(express.static('/css'));
+// app.use(express.static('/js'));
+// app.use(express.static('/bower_components'));
+// app.use(express.static('/data'));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/external.html', function(req, res) {
+    res.sendFile(path.join(__dirname + '/external.html'));
+});
+
+// app.get('/js/app.js',function(req,res){
+//     res.sendFile(path.join(__dirname + '/js/app.js'));
+//Express Middleware for serving static files
+app.use('/public', express.static(path.join(__dirname, '/public')));
+// viewed at http://localhost:8080
+
+
+
+
+
+
+app.listen(8080);
