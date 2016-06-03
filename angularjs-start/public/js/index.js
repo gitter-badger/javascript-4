db4ff889d4c88fdba405ca53da7646c26bd4724b
aksinghdce <aksinghdce@gmail.com>
2016-06-03 19:05:49 +0530

    UI automation done

--- angularjs-start/public/js/index.js
+++ angularjs-start/public/js/index.js
@@ -42,61 +42,12 @@
                       create_elements_with(element, scope.content);
                       // console.log(scope.content);
                       // console.log(scope.ids);
-                        activate_textbox();
-                        activate_textbox2();
-
-
-                        $('#dialog').on('refresh-content', function() {
-                            console.log('refresh called');
-                            $('#dialog').modalPopover('hide');
-
-                            $('#dialog').modalPopover('show');
-                            set_popover_style('#dialog');
-
-                        });
-
-                        $('#dialog2').on('refresh-content', function() {
-                            console.log('refresh called');
-                            $('#dialog2').modalPopover('hide');
-
-                            $('#dialog2').modalPopover('show');
-                            set_popover_style('#dialog2');
-
-                        });
-
-                        $('#the-text').on("bubble-selected", function(event, data, size, disabled) {
-                            console.log(data);
-                            var duplicate = false;
-                            $(this).find('option').each(function() {
-                                if (this.text == data) {
-                                    console.log("match found, not adding");
-                                    duplicate = true;
-                                }
-                            });
-
-                            if (!duplicate) {
-                                var option = $('<option>')
-                                    .attr('selected', 'selected')
-                                    .attr('value', size);
-
-                                if (disabled) {
-                                    option.attr("disabled", "disabled");
-                                }
-                                option.text(data);
-                                $('#the-text').append(option);
-                            }
-
-                        });
-
                         $('select').select2({
                             tags: true,
                             tokenSeparators: [',', ' ']
                         });
-
-
                         $('body').css('overflow-x', 'auto');
                         $('body').css('overflow-y', 'auto');
-
                         //uncomment the line below to disable select2 scrolldown menu
                         //$(this).select2('close');
                     }
@@ -112,6 +63,79 @@
         console.log("data is select2");
         $('<select class="js-basic-multiple js-states form-control" id="the-text" multiple="multiple"></select>')
         .appendTo(element);
+
+        var modal_inner = $('<div id="external-content" class="external-popover popover-content"></div>');
+        var modal_outer = $('<div id="dialog" class="popover">')
+        modal_inner.appendTo(modal_outer);
+        modal_outer.appendTo('#search-results');
+
+
+        $('#the-text').on('select2:open', function() {
+
+
+            var options = {
+                target: "#external-content",
+                remote: "external.html",
+                placement: "right",
+                backdrop: "false",
+                keyboard: "true"
+            };
+            $('#dialog').modalPopover(options);
+            $('#dialog').modalPopover('show');
+            $('#dialog').css('overflow-y', 'auto');
+            $('#dialog').css('overflow-x', 'auto');
+            $('#dialog').css('position', 'relative');
+            $('#dialog').css('border', '3px solid #8AC007');
+            $('#dialog').css('top', '0px');
+            $('#dialog').css('left', '0px');
+            $('#dialog').css('max-width', '1000px');
+            $('#dialog').css('max-height', '90%');
+
+            //uncomment the line below to disable select2 scrolldown menu
+            //$(this).select2('close');
+        });
+
+        $('#the-text').on("bubble-selected", function(event, data, size, disabled) {
+            console.log(data);
+            var duplicate = false;
+            $(this).find('option').each(function() {
+                if (this.text == data) {
+                    console.log("match found, not adding");
+                    duplicate = true;
+                }
+            });
+
+            if (!duplicate) {
+                var option = $('<option>')
+                    .attr('selected', 'selected')
+                    .attr('value', size);
+
+                if (disabled) {
+                    option.attr("disabled", "disabled");
+                }
+                option.text(data);
+                $('#the-text').append(option);
+            }
+
+        });
+
+        $('#dialog').on('refresh-content', function() {
+            console.log('refresh called');
+            $('#dialog').modalPopover('hide');
+
+            $('#dialog').modalPopover('show');
+            $('#dialog').css('overflow-y', 'auto');
+            $('#dialog').css('overflow-x', 'auto');
+            $('#dialog').css('position', 'relative');
+            $('#dialog').css('border', '3px solid #8AC007');
+            $('#dialog').css('top', '0px');
+            $('#dialog').css('left', '0px');
+            $('#dialog').css('max-width', '1000px');
+            $('#dialog').css('max-height', '90%');
+
+        });
+
+
         console.log("done");
       }
     }
@@ -135,61 +159,6 @@
             angular.bootstrap(document, ["myApplication"]);
         });
     }
-
-
-    var set_popover_style = function(dialog) {
-        $(dialog).css('overflow-y', 'auto');
-        $(dialog).css('overflow-x', 'auto');
-        $(dialog).css('position', 'relative');
-        $(dialog).css('border', '3px solid #8AC007');
-        $(dialog).css('top', '0px');
-        $(dialog).css('left', '0px');
-        $(dialog).css('max-width', '1000px');
-        $(dialog).css('max-height', '90%');
-    }
-
-    var activate_textbox = function() {
-        $('#the-text').on('select2:open', function() {
-
-
-            var options = {
-                target: "#external-content",
-                remote: "external.html",
-                placement: "right",
-                backdrop: "false",
-                keyboard: "true"
-            };
-            $('#dialog').modalPopover(options);
-            $('#dialog').modalPopover('show');
-            set_popover_style('#dialog');
-
-            //uncomment the line below to disable select2 scrolldown menu
-            //$(this).select2('close');
-        });
-    }
-
-
-    var activate_textbox2 = function() {
-        $('#the-text2').on('select2:open', function() {
-
-            var options = {
-                target: "#external-content",
-                remote: "map.html",
-                placement: "right",
-                backdrop: "false",
-                keyboard: "true"
-            };
-            $('#dialog2').modalPopover(options);
-            $('#dialog2').modalPopover('show');
-            set_popover_style('#dialog2');
-            //uncomment the line below to disable select2 scrolldown menu
-            //$(this).select2('close');
-        });
-    }
-
-    // $('#the-text').on('select2:selected', function (event, data) {
-    //   console.log(event + " " + data);
-    // });
 
 
 
