3dd1a117ca7a3788646bc3836429c97f781c1cc2
aksinghdce <aksinghdce@gmail.com>
2016-06-05 13:25:01 +0530

    Freecodecamp wikipedia application done

--- wikipedia_searcher/js/index.js
+++ wikipedia_searcher/js/index.js
@@ -4,15 +4,14 @@ $(document).on('ready', function(){
       window.open('https://en.wikipedia.org/wiki/Special:Random', 'Random Wiki Article', 'Window Settings');
     });
 
-    var callback_fn = function(data_got_from_jsonp) {
-      alert(data_got_from_jsonp.length);
-    };
 
   $("#search-button")
   .on('click',
      function(){
+
+       //generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=
     var base_url = "https://en.wikipedia.org/w/api.php";
-    var main_page = "action=query&format=json&prop=images" +"&titles=" +
+    var main_page = "action=query&format=json&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=" +
      $("#search-church").val()+ "&callback=?";
 
     // Assign handlers immediately after making the request,
@@ -21,7 +20,21 @@ var jqxhr = $.getJSON( base_url+"?"+main_page, function() {
   console.log( "success" );
 })
   .done(function(data) {
-    console.log( data );
+    console.log(data);
+    if(data.query.pages.length == 0 ){
+      $('#wiki-output').html("No results found. Please try modifying your search string");
+    }
+    $('#wiki-output').html('');
+    var results = data.query.pages;
+    $.each(results, function(pageid, page){
+      var title = $('<a>').html(page.title)
+      .attr('href', 'https://en.wikipedia.org/?curid=' + pageid);
+      title.appendTo("#wiki-output");
+      $('<br>').appendTo("#wiki-output");
+      $('<p>').html(page.extract).appendTo("#wiki-output");
+      $('<br>').appendTo("#wiki-output");
+    });
+
   })
   .fail(function(error) {
     console.log( "ERROR OCCURRED:" + String(error) );
